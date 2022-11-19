8e7675711c79e278d4e3e93b1d6fceaf7c2c7f0f
EsmeraldaFrey <esmefrey@gmail.com>
2022-11-19 17:18:09 -0300

    prueba

--- BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Controller/PersonaController.java
+++ BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Controller/PersonaController.java
@@ -19,7 +19,7 @@ public class PersonaController {
     @Autowired IPersonaService ipersonaService; 
     
     
-    @GetMapping("personas/traer")
+    @GetMapping("/personas/traer")
     public List<Persona> getPersona(){
         return ipersonaService.getPersona();
     }
@@ -50,5 +50,10 @@ public class PersonaController {
         ipersonaService.savePersona(persona);
         
         return persona;
+    }
+    
+    @GetMapping("/personas/traer/perfil")
+    public Persona findPersona(){
+        return ipersonaService.findPersona((long)1);
     }
 }
