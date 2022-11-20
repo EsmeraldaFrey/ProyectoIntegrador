5d234ef0b415191e20174f0a0fb3003902370e1f
EsmeraldaFrey <esmefrey@gmail.com>
2022-11-20 20:39:47 -0300

    Este es el mensaje correcto

--- BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Controller/CHys.java
+++ BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Controller/CHys.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 //@CrossOrigin(origins = "http://localhost:4200")
-@CrossOrigin(origins = "https://mgbfrontend.web.app")
+@CrossOrigin(origins = "https://portfolioesme.web.app")
 @RequestMapping("/skill")
 public class CHys {
 
--- BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Security/Repository/iRolRepository.java
+++ BackEnd/src/main/java/com/Proyecto_Integrador/EsmeF/Security/Repository/iRolRepository.java
@@ -11,7 +11,7 @@ package com.Proyecto_Integrador.EsmeF.Security.Repository;
  * @author Esme
  */
 import com.Proyecto_Integrador.EsmeF.Security.Entity.Rol;
-import com.Proyecto_Integrador.EsmeF.Enums.RolNombre;
+import com.Proyecto_Integrador.EsmeF.Security.Enums.RolNombre;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
